764db053664bc7186b401bbcd2e3de0a95a54c9c
KesaubeEire <760093121@qq.com>
2018-10-13 11:34:27 +0800

    走你

---
+++ Carrot_Fantasy/Assets/CarrotFantasyMain/Scripts/FrameWork/Pool/IReusable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//对象池接口
+public interface IReusable
+{
+    //取出
+    void OnSpawn();
+
+    //回收
+    void OnUnSpawn();
+}
---
+++ Carrot_Fantasy/Assets/CarrotFantasyMain/Scripts/FrameWork/Pool/ReusableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///为保证这两个函数一定在子函数中实现
+/// 需要这样写上abstract并且不带函数体(即那两个大括号 '{'  && '}'
+public abstract class ReusableObject : MonoBehaviour, IReusable
+{
+    public abstract void OnSpawn();
+    public abstract void OnUnSpawn();
+}
